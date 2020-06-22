ec5843df3c3926a5e77e35ffabe1c199b55341e3
gokul-prathap <gokulprathap@rocketmail.com>
2020-06-23 02:07:32 +0530

    Updates

---
+++ precache-manifest.ec9877f07793c770c83fcca4b17ebe85.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfa0a044c732d3619f1f9956abf3082",
+    "url": "/cadoart/index.html"
+  },
+  {
+    "revision": "2a0c66fed2336220dd9c",
+    "url": "/cadoart/static/css/main.7b65a004.chunk.css"
+  },
+  {
+    "revision": "19d23724cedd78ecf0d6",
+    "url": "/cadoart/static/js/2.b0d0155b.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/cadoart/static/js/2.b0d0155b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0c66fed2336220dd9c",
+    "url": "/cadoart/static/js/main.bc819d8c.chunk.js"
+  },
+  {
+    "revision": "3f42699d8a4e708f2272",
+    "url": "/cadoart/static/js/runtime-main.4aa795f2.js"
+  },
+  {
+    "revision": "7257a1a94be550133f3233fd792ee724",
+    "url": "/cadoart/static/media/avatar.7257a1a9.jpg"
+  },
+  {
+    "revision": "6b1983a26a32647c4832456dc9f5dcd3",
+    "url": "/cadoart/static/media/bg1.6b1983a2.jpg"
+  },
+  {
+    "revision": "f807b37b1736e7709918ea35ef80ca7d",
+    "url": "/cadoart/static/media/bg3.f807b37b.jpg"
+  },
+  {
+    "revision": "056eb0586182a8d28273ad11c028e912",
+    "url": "/cadoart/static/media/bg5.056eb058.jpg"
+  },
+  {
+    "revision": "d7d1aaf71d275992031bd75cdb6ec65c",
+    "url": "/cadoart/static/media/bg6.d7d1aaf7.jpg"
+  },
+  {
+    "revision": "089c4f4e55f158a69d0364c6b6339f37",
+    "url": "/cadoart/static/media/eva.089c4f4e.jpg"
+  },
+  {
+    "revision": "aa57f0508d8b419125067c5cfac981e6",
+    "url": "/cadoart/static/media/header.aa57f050.jpg"
+  },
+  {
+    "revision": "09fe08725aef892d28e6e2ad52700b32",
+    "url": "/cadoart/static/media/img (10).09fe0872.jpg"
+  },
+  {
+    "revision": "a5f269dcffc224863578748c6bd50991",
+    "url": "/cadoart/static/media/img (12).a5f269dc.jpg"
+  },
+  {
+    "revision": "4e0e604520fcef6b2044c96fe87a11f8",
+    "url": "/cadoart/static/media/img (15).4e0e6045.jpg"
+  },
+  {
+    "revision": "cbedb284e1ae047d242d74ec7374acb9",
+    "url": "/cadoart/static/media/img (16).cbedb284.jpg"
+  },
+  {
+    "revision": "88c90dc72172cddad90fb1c53c618ce2",
+    "url": "/cadoart/static/media/img (17).88c90dc7.jpg"
+  },
+  {
+    "revision": "f8e7cc869056f9b6ba70fa07fd6c327e",
+    "url": "/cadoart/static/media/img (19).f8e7cc86.jpg"
+  },
+  {
+    "revision": "9f8587386b2dfce7cb49d260de061bd7",
+    "url": "/cadoart/static/media/img (23).9f858738.jpg"
+  },
+  {
+    "revision": "b54ae26897ad524d2429eb1bb47b1425",
+    "url": "/cadoart/static/media/img (24).b54ae268.jpg"
+  },
+  {
+    "revision": "16c3ef04cdeb3098c97c686533144777",
+    "url": "/cadoart/static/media/img (25).16c3ef04.jpg"
+  },
+  {
+    "revision": "2520b48051420b1a884884b42f4e49d8",
+    "url": "/cadoart/static/media/img (26).2520b480.jpg"
+  },
+  {
+    "revision": "d4af83d4902fbc72da9e35d6c3259d93",
+    "url": "/cadoart/static/media/img (27).d4af83d4.jpg"
+  },
+  {
+    "revision": "00dd81be16ae2ec75e3c84b76abdd3ba",
+    "url": "/cadoart/static/media/img (28).00dd81be.jpg"
+  },
+  {
+    "revision": "767c40e6f4a8f526c60041a3f7aca18f",
+    "url": "/cadoart/static/media/img (3).767c40e6.jpg"
+  },
+  {
+    "revision": "8a001e7dba1264e6574a5ac83d30dee7",
+    "url": "/cadoart/static/media/img (32).8a001e7d.jpg"
+  },
+  {
+    "revision": "1d86a6d36ec1f15089ead9300d95b2ac",
+    "url": "/cadoart/static/media/img (33).1d86a6d3.jpg"
+  },
+  {
+    "revision": "44831e6935258c6abd4c7fd103ca7266",
+    "url": "/cadoart/static/media/img (4).44831e69.jpg"
+  },
+  {
+    "revision": "37d49d3848db5e61ca2919413dd192a5",
+    "url": "/cadoart/static/media/img (7).37d49d38.jpg"
+  },
+  {
+    "revision": "612b0b6704856383846a0896cac15965",
+    "url": "/cadoart/static/media/img (8).612b0b67.jpg"
+  },
+  {
+    "revision": "b21019516fd574253e2354080dc1dd00",
+    "url": "/cadoart/static/media/login.b2101951.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/cadoart/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/cadoart/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/cadoart/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/cadoart/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "f217f346c88e5fb2720ca7c28827ff72",
+    "url": "/cadoart/static/media/ryan.f217f346.jpg"
+  }
+]);
